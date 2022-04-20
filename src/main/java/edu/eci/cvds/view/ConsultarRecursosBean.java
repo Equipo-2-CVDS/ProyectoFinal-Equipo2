53767ffe57f700d135aa046938857cf061aed1cd
sergio.otero <sergioaotero26@hotmail.com>
2022-04-20 18:37:49 -0500

    Intentando arreglar dataTable

--- src/main/java/edu/eci/cvds/view/ConsultarRecursosBean.java
+++ src/main/java/edu/eci/cvds/view/ConsultarRecursosBean.java
@@ -18,13 +18,65 @@ import java.util.List;
 public class ConsultarRecursosBean extends BasePageBean{
     @Inject
     private ProyectoServices userServices;
+    private int id;
+    private String nombre;
+    private String tipo;
+    private int capacidad;
+    private String ubicacion;
+    private boolean estado=true;
 
     public List<Recurso> consultarRecursos() throws ServicesException {
         return userServices.getRecursosDisponibles();
     }
 
-    public List<Recurso> getRecursoFromTipo(String tipo) throws ServicesException {
+    public List<Recurso> getRecursoPorFiltro(String tipo, int capacidad, String ubicacion) throws ServicesException {
         return userServices.getRecursosFromTipo(tipo);
     }
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
 }
