8e071493f48aa6477b81ae00006b929bb1ad8c6c
juanbaezl <juan.baez-l@mail.escuelaing.edu.co>
2022-04-29 09:53:21 -0500

    Modificacion tabla recurrencia

--- src/main/java/edu/eci/cvds/entities/Reserva.java
+++ src/main/java/edu/eci/cvds/entities/Reserva.java
@@ -8,20 +8,23 @@ public class Reserva {
     private int idRecurso;
     private Timestamp desde;
     private Timestamp hasta;
+    private int recurrencia;
 
-    public Reserva(int id, int idUsuario, int idRecurso, Timestamp desde, Timestamp hasta) {
+    public Reserva(int id, int idUsuario, int idRecurso, Timestamp desde, Timestamp hasta, int recurrencia) {
         this.id = id;
         this.idUsuario = idUsuario;
         this.idRecurso = idRecurso;
         this.desde = desde;
         this.hasta = hasta;
+        this.recurrencia = recurrencia;
     }
 
-    public Reserva(int idUsuario, int idRecurso, Timestamp desde, Timestamp hasta) {
+    public Reserva(int idUsuario, int idRecurso, Timestamp desde, Timestamp hasta, int recurrencia) {
         this.idUsuario = idUsuario;
         this.idRecurso = idRecurso;
         this.desde = desde;
         this.hasta = hasta;
+        this.recurrencia = recurrencia;
     }
 
     public int getId() {
@@ -64,9 +67,17 @@ public class Reserva {
         this.hasta = hasta;
     }
     
+    public int getRecurrencia() {
+        return recurrencia;
+    }
+
+    public void setRecurrencia(int recurrencia) {
+        this.recurrencia = recurrencia;
+    }
+
     @Override
     public String toString() {
         return "Recurso: [id=" + id + ", idUsuario=" + idUsuario + 
-        ", idRecurso=" + idRecurso +  ", desde=" + desde + ", hasta=" + hasta +"]";
+        ", idRecurso=" + idRecurso +  ", desde=" + desde + ", hasta=" + hasta + ", recurrencia=" + recurrencia +"]";
     }
 }
