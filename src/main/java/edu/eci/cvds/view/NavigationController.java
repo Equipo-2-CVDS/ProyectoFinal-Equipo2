7879da9b2d77e09005b35aea864646cfad8b6c10
nicolaspalacios-f <nicolas.palacios-f@mail.escuelaing.edu.co>
2022-05-16 18:16:21 -0500

    boton home

--- src/main/java/edu/eci/cvds/view/NavigationController.java
+++ src/main/java/edu/eci/cvds/view/NavigationController.java
@@ -3,37 +3,47 @@ package edu.eci.cvds.view;
 import java.io.IOException;
 import java.io.Serializable;
 
-import javax.faces.bean.ManagedBean; 
+import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
-
 
 @ManagedBean(name = "navigationController", eager = true)
 @RequestScoped
 
 public class NavigationController implements Serializable {
-    public void paginaHome() throws IOException{
+    public void paginaHome() throws IOException {
         FacesContext.getCurrentInstance().getExternalContext().redirect("/faces/home.xhtml");
     }
-    public void paginaLogin() throws IOException{
+
+    public void paginaLogin() throws IOException {
         FacesContext.getCurrentInstance().getExternalContext().redirect("/faces/login.xhtml");
     }
-    public void paginaInicio() throws IOException{
+
+    public void paginaInicio() throws IOException {
         FacesContext.getCurrentInstance().getExternalContext().redirect("/faces/index.xhtml");
     }
-    public void paginaConsultaR() throws IOException{
+
+    public void paginaConsultaR() throws IOException {
         FacesContext.getCurrentInstance().getExternalContext().redirect("/faces/consultarRecurso.xhtml");
     }
-    public void paginaRegistroR() throws IOException{
+
+    public void paginaRegistroR() throws IOException {
         FacesContext.getCurrentInstance().getExternalContext().redirect("/faces/registrarRecurso.xhtml");
     }
-    public void paginarecursoR() throws IOException{
+
+    public void paginarecursoR() throws IOException {
         FacesContext.getCurrentInstance().getExternalContext().redirect("/faces/consultarReserva.xhtml");
     }
-    public void paginaMisReservas() throws IOException{
+
+    public void paginaMisReservas() throws IOException {
         FacesContext.getCurrentInstance().getExternalContext().redirect("/faces/misReservas.xhtml");
     }
-    public void paginaCalendario() throws IOException{
+
+    public void paginaCalendario() throws IOException {
         FacesContext.getCurrentInstance().getExternalContext().redirect("/faces/calendario.xhtml");
+    }
+
+    public void paginaReporte() throws IOException {
+        FacesContext.getCurrentInstance().getExternalContext().redirect("/faces/estadisticas.xhtml");
     }
 }
